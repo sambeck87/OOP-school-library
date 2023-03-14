b39cd15912345bbfa5e7c648d91e60d761221d4b
diego <dgcuenca@gmail.com>
2023-03-14 10:32:59 -0500

    Trying to resolve write properly JSON

--- save_data.rb
+++ save_data.rb
@@ -1,7 +1,12 @@
+require 'json'
+
 def save_people(people)
   File.open('people.json', 'w') do |file|
-    file.write people
+    file.write JSON.generate(people)
   end
+  json_string = File.read('people.json')
+  data = JSON.parse(json_string)
+  puts data
 end
 
 def save_book(book)
@@ -12,6 +17,35 @@ end
 
 def save_rental(rental)
   File.open('rentals.json', 'w') do |file|
-    file.write rental
+    file.write JSON.generate(rental)
   end
 end
+
+# require 'json'
+
+# # Read the file into a string
+# json_string = File.read('file.json')
+
+# # Parse the JSON string into a hash
+# data = JSON.parse(json_string)
+
+# # Access the data as needed
+# puts data['key1']
+# puts data['key2']['nested_key']
+
+# require 'json'
+
+
+# # Data to be written to the file
+# data = {
+#   "name" => "John",
+#   "age" => 30,
+#   "email" => "john@example.com"
+# }
+
+# # Convert the data to a JSON-formatted string
+# json_string = JSON.generate(data)
+
+# # Write the JSON string to a file
+# File.write('file.json', json_string)
+
